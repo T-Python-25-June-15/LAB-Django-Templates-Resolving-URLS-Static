6aac46ed6d5f15edd464c0d9d1d17f27636a4ca6
shatha <shatha25200@gmail.com>
2025-07-07 20:43:43 +0300

    Done

---
+++ GoogleSearch/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+# Create your views here.
+
+
+
+def home_page(request:HttpRequest):
+    return render(request, 'main/google.html')
+
+def terms_page(request:HttpRequest):
+    return render(request, 'main/terms.html')
